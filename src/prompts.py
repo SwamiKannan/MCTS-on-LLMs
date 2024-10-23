a214b19fdba20fa72c6cbef7f9810d7e83690a02
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-10-23 13:01:57 +0530

    Update prompts.py
    Added appropriate spaces since merging the prompt  not create the correct text
    Additional import for process_single_prompt()
    Updated process_single_prompt() to return the correct text
    Updated rate answer to extract critique of answer

--- src/prompts.py
+++ src/prompts.py
@@ -1,4 +1,5 @@
 import re
+from llm_call_llama_cpp import process_multiprompt, process_single_prompt
 
 
 seed_answers = [
@@ -13,47 +14,59 @@ def chat_completion_request(prompt):
     # return the content of LLM response with temperature 1.0 and max_tokens = 1500
 
 def get_critique(question, draft_answer):
-    prompt = (f'Question: {question}\n',
-              f'Draft answer: {draft_answer}\n'
-              'Please critique the draft answer'
-              'Do a careful assessment of whether the answer is correct or not, and why',
-              'Consider multiple ways of verifying the correctness of the answer',
-              'Do point out every flaw and hold the draft answer to a high standard',
-              'Do provide specific recommendations to improve the answer',
-              'Do think step by step',
-              'Do not provide a revised answer'
-    )
+    prompt = f'Question: {question}\n '\
+              f'Draft answer: {draft_answer}\n '\
+              'Please critique the draft answer '\
+              'Do a careful assessment of whether the answer is correct or not, and why '\
+              'Consider multiple ways of verifying the correctness of the answer '\
+              'Do point out every flaw and hold the draft answer to a high standard '\
+              'Do provide specific recommendations to improve the answer '\
+              'Do think step by step '\
+              'Do not provide a revised answer '
+    
 
-    return chat_completion_request(prompt)
+    return process_single_prompt(prompt, {'temperature':1, 'max_tokens':1500})
 
 def improve_answer(question, draft_answer, critique):
+    prompt = f'Question: {question}\n' \
+        f'Answer: {draft_answer}\n' \
+        f'Critique: {critique}\n' \
+        'Please improve the draft answer based on the critique. Follow this format: '\
+        'Reasoning process: <step-by-step reasoning process>\n'\
+        'Verification: <verification of the facts>\n'\
+        'Final Answer: <the improved and verified answer>\n'
+    
+    return process_single_prompt(prompt, {'temperature':1, 'max_tokens':1500})
+
+
+def next_step(question, draft_answer, critique):
     prompt = (
         f'Question: {question}\n'
         f'Answer: {draft_answer}\n'
         f'Critique: {critique}\n',
-        'Please improve the draft answer based on the critique. Follow this format'
+        'What is the next step of your solution. Follow this format '
         'Reasoning process: <step-by-step reasoning process>\n'
         'Verification: <verification of the facts>\n'
         'Final Answer: <the improved and verified answer>\n'
     )
-    return chat_completion_request(prompt)
+    return process_single_prompt(prompt, {'temperature':1, 'max_tokens':1500})
 
 def rate_answer(question, answer):
     prompt = (
-        f'Question: {question}\n'
-        f'Answer: {answer}\n\n'
-        "Assess whether the user's query requires only the final answer or whether a justification is required"
-        'As an expert on this topic, please suggest a detailed critique of the answer, pointing out every flaw'
-        'Provide only a critique, not a suggested answer'
-        'Then, rate the answer on a scale of 0 to 100'
-        'The response should be in the following format:\n',
-        'Critique: <detailed critique>\n'
+        f'Question: {question}\n '
+        f'Answer: {answer}\n\n '
+        "Assess whether the user's question requires only the final answer or whether a justification is required "
+        'As an expert on this topic, please suggest a detailed critique of the answer, pointing out every flaw '
+        'Provide only a critique, not a suggested answer '
+        'Then, rate the answer on a scale of 0 to 100 '
+        'The response should be in the following format:\n ',
+        'Critique: <detailed critique>\n '
         'Rating: <rating>'
         )
     
-    rating_response = chat_completion_request(prompt)
+    response = process_single_prompt(prompt, {'temperature':1, 'max_tokens':1500})
     try:
-        match = re.search(r'Rating:\s*(\d+)', rating_response)
+        match = re.search(r'Rating:\s*(\d+)', response)
         if match:
             rating  = int(match.group(1))
             if rating > 95:
@@ -63,9 +76,20 @@ def rate_answer(question, answer):
             raise ValueError('Rating not found in the response')
     except Exception  as e:
         print(f'Error in extracting rating: {e}')
-        print(f'Rating response was: {rating_response}')
+        print(f'Rating response was: {response}')
         rating = 0.0
-    return rating
+
+    try:
+        match = re.search(r'Critique:\s*(\d+)', response)
+        if match:
+            critique  = match.group(1)
+        else:
+            raise ValueError('Critique not found in the response')
+    except Exception  as e:
+        print(f'Error in extracting critique: {e}')
+        print(f'Rating response was: {response}')
+        critique = ''
+    return rating, critique
 
 def get_answer_directly_from_llm(question):
     prompt = (
