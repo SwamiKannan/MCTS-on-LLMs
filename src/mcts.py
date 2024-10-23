b10e05b4e39d1b0fac0b5ae68e8edba3ca00cc6c
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-10-23 16:26:23 +0530

    Update mcts.py
    Add initial variables for an MCTS Node class

--- src/mcts.py
+++ src/mcts.py
@@ -14,3 +14,17 @@ extra_body = {"use_beam_search": True,
 "best_of": 1
 }
 samplers = [{'temperature':0.0, 'extra_body':extra_body}, {'temperature':0.8, 'top_p':0.7}, {'temperature':1.2, 'top_k':20}]
+
+class Node():
+    def __init__(self, id, question, answer,parent=None):
+        self.id = str(id)
+        self.level = parent.level + 1 if parent else 0
+        self.parent = parent
+        self.children = []
+        self.total_visits = 0
+        self.episilon = EPSILON_CONSTANT
+        self.score = float('inf')
+        self.response = None
+        self.question = question
+        self.answer = answer
+        self.sampler = samplers[int(id.split('_')[-1])]
